fee29d4d33a4c1e1555dae3878a84d7c1bd38f62
vanshika saxena <saxenavanshika979@gmail.com>
2024-08-12 18:51:47 +0530

    Update file

---
+++ TRAVEL-WEBSITE/index.js
@@ -0,0 +1,60 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import authRoute from "./router/auth.js";
+import tourRoute from "./router/tours.js";
+import userRoute from "./router/users.js";
+import reviewRoute from "./router/review.js";
+import bookingRoute from "./router/bookings.js";
+import searchRoute from "./router/Search.js";
+import contactRoute from "./router/contact.js";
+import blogRoute from "./router/blog.js";
+import commentRoute from "./router/comment.js";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+mongoose.set("strictQuery", false);
+
+async function connect() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("MongoDB Database Connected");
+  } catch (err) {
+    console.log("MongoDB Database Connection Failed");
+  }
+}
+
+const corsOptions = {
+  origin: true,
+  credentials: true,
+};
+
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/tours", tourRoute);
+app.use("/api/v1/search", searchRoute);
+app.use("/api/v1/users", userRoute);
+app.use("/api/v1/review", reviewRoute);
+app.use("/api/v1/booking", bookingRoute);
+app.use("/api/v1/contact", contactRoute);
+app.use("/api/v1/blogs", blogRoute);
+app.use("/api/v1/comment", commentRoute);
+
+app.listen(port, () => {
+  connect();
+  console.log("Server is listening on port", port);
+});
